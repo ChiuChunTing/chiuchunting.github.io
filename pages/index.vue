8595232e44d9c4e646caff3aa6c91d940e326fe0
IngridKao <ingrid-kao@qware.com>
2025-04-25 00:36:07 +0800

    Remove unused SEO meta handling from index.vue

--- pages/index.vue
+++ pages/index.vue
@@ -3,15 +3,7 @@ import TopSection from '@/assets/components/home/TopSection.vue'
 import IntroductionSection from '@/assets/components/home/IntroductionSection.vue'
 import ExhibitionSection from '@/assets/components/home/ExhibitionSection.vue'
 import WebFooter from '@/assets/components/layout/WebFooter.vue'
-const { t, locale } = useI18n()
-const lang = computed(() => locale.value)
-watch(lang, () => {
-  useSeoMeta({
-    description: t('home.seo.description'),
-    ogDescription: t('home.seo.description'),
-    twitterDescription: t('home.seo.description'),
-  })
-}, { immediate: true })
+
 </script>
 
 <template>
